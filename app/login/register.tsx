8e297a1788539ee6b27aa5f50a8b5e4a7f331a75
thiagotnaraujo <thago@crowdability.com>
2025-01-08 20:01:35 -0300

    Login

--- app/login/register.tsx
+++ app/login/register.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import { Button, Text, TextInput, View } from 'react-native'
+import styles from './styles'
 
 export default function Register(): JSX.Element {
   return (
-    <View>
-      <Text>Register</Text>
+    <View style={styles.screen}>
+      <View style={styles.loginBox}>
+        <Text style={styles.loginTitle}>Crie seu usuário</Text>
+        <Text style={styles.label}>E-mail</Text>
+        <TextInput style={styles.input} />
+        <Text style={styles.label}>Senha</Text>
+        <TextInput style={styles.input} />
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Entrar"
+            color="#4e46dd"
+            onPress={() => console.log('Simple Button pressed')}
+          />
+        </View>
+      </View>
     </View>
   )
 }
