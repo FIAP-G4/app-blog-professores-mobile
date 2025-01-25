3a983e4dbe78a723b2fcf00b9fadfde6e30e41c0
Renan Pedrosa <renandspedrosa@gmail.com>
2025-01-25 00:11:42 -0300

    criando usuario

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = process.env.CORS_ORIGIN || 'http://192.168.0.54:3000'
+const baseURL = process.env.CORS_ORIGIN || 'http://192.168.18.30:3000/'
 
 const api = axios.create({
   baseURL: baseURL,
