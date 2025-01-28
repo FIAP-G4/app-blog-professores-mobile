3d854d14519741486ea8f84345f6b3f3369c8760
pedrovbo <pedrovicboliveira@gmail.com>
2025-01-28 06:22:30 -0300

    Adição de botão de login no header e ação de esconder o botão caso esteja na rota login

--- app/components/shared/Header/index.tsx
+++ app/components/shared/Header/index.tsx
@@ -1,6 +1,7 @@
 import React, { createContext } from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, Pressable } from 'react-native'
 import styles from './styles'
+import { useRouter, useSegments } from 'expo-router'
 
 interface HeaderProps {
   pageTitle: string
@@ -12,6 +13,16 @@ export const HeaderContext = createContext<HeaderProps | null>(null)
 export default function Header(props: HeaderProps): JSX.Element {
   const { pageTitle, children } = props
   const logoPath = '../../../../assets/images/logo.png'
+  const router = useRouter()
+  const segments = useSegments() as string[]
+
+  const handleLogin = () => {
+    router.push('/login')
+  }
+
+  const isLoginRoute = segments.includes('login')
+
+  console.log(isLoginRoute)
 
   return (
     <HeaderContext.Provider value={props}>
@@ -19,6 +30,11 @@ export default function Header(props: HeaderProps): JSX.Element {
         <Image style={styles.headerLogo} source={require(logoPath)} />
         <Text style={styles.headerText}>{pageTitle}</Text>
         {children}
+        {!isLoginRoute && (
+          <Pressable onPress={handleLogin} style={styles.loginButton}>
+            <Text style={styles.loginButtonText}>Login</Text>
+          </Pressable>
+        )}
       </View>
     </HeaderContext.Provider>
   )
