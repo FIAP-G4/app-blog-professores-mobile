4214d13cce846d5cfe87dafa312867e9ca23a6cd
Renan Pedrosa <renandspedrosa@gmail.com>
2025-01-28 16:42:47 -0300

    melhoria service teacher e student

---
+++ app/services/user/user.ts
@@ -0,0 +1,50 @@
+import api from '@/app/services/api'
+
+interface User {
+  id: string
+  name: string
+  email: string
+  password: string
+}
+
+export const findUserByEmail = async (email: string): Promise<User> => {
+  try {
+    const response = await api.get<User>(`/user/email/${email}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    return response.data
+  } catch (error: any) {
+    console.error(
+      'Erro ao encontrar o usuário:',
+      error.response ? error.response.data : error.message,
+    )
+    throw error
+  }
+}
+
+export const updateUser = async (
+  id: string,
+  param: Partial<User>,
+): Promise<User> => {
+  try {
+    const token = localStorage.getItem('authToken')
+
+    const response = await api.put<User>(`/user/${id}`, param, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    return response.data
+  } catch (error: any) {
+    console.error(
+      'Erro ao atualizar usuário:',
+      error.response ? error.response.data : error.message,
+    )
+    throw error
+  }
+}
