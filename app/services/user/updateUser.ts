873928bf19767ec73c416647e28c5c02b96a3f75
Renan Pedrosa <renandspedrosa@gmail.com>
2025-01-28 16:48:37 -0300

    melhorando servico user

--- app/services/user/updateUser.ts
+++ app/services/user/updateUser.ts
@@ -1,29 +1,5 @@
 import api from '@/app/services/api'
-
-interface User {
-  id: string
-  name: string
-  email: string
-  password: string
-}
-
-export const findUserByEmail = async (email: string): Promise<User> => {
-  try {
-    const response = await api.get<User>(`/user/email/${email}`, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-
-    return response.data
-  } catch (error: any) {
-    console.error(
-      'Erro ao encontrar o usuário:',
-      error.response ? error.response.data : error.message,
-    )
-    throw error
-  }
-}
+import { User } from './Iuser'
 
 export const updateUser = async (
   id: string,
