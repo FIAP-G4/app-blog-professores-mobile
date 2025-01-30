4c8cc40cc9c53ed3e05411af2c1ee2b660bb5157
thiagotnaraujo <thago@crowdability.com>
2025-01-30 18:21:26 -0300

    Remoção de código comentado

--- app/(screens)/postagens/_layout.tsx
+++ app/(screens)/postagens/_layout.tsx
@@ -1,17 +1,9 @@
-import { Slot, usePathname, useSegments } from 'expo-router'
+import { Slot } from 'expo-router'
 import React from 'react'
 import { Text, View } from 'react-native'
 import styles from './styles'
 
 export default function PostsLayout() {
-  // const pathName = usePathname()
-  // const segments = useSegments()
-  // const screenName =
-  //   segments.length > 1
-  //     ? segments.slice(-1)[0].charAt(0).toUpperCase() +
-  //       segments.slice(-1)[0].slice(1)
-  //     : pathName.charAt(1).toUpperCase() + pathName.slice(2)
-
   return (
     <>
       <View style={styles.subHeader}>
