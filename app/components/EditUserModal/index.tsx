54233db8a7851e9a572dd550f9c516cda11d8b83
Renan Pedrosa <renandspedrosa@gmail.com>
2025-02-01 13:59:20 -0300

    schema

--- app/components/EditUserModal/index.tsx
+++ app/components/EditUserModal/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Modal, View, Text, TextInput, Button, Switch } from 'react-native';
 import { User } from '@/app/services/user/IUser';
 import { Formik } from 'formik';
-import * as Yup from 'yup';
 import styles from './styles';
+import schema from './schema';
 
 interface EditUserModalProps {
   visible: boolean;
@@ -11,25 +11,6 @@ interface EditUserModalProps {
   onClose: () => void;
   onSave: (updatedUser: User) => void;
 }
-
-const schema = Yup.object().shape({
-  name: Yup.string().required('Nome é obrigatório'),
-  email: Yup.string().email('E-mail inválido').required('E-mail é obrigatório'),
-  changePassword: Yup.boolean(),
-  password: Yup.string().when('changePassword', {
-    is: true,
-    then: (schema) => schema.min(6, 'A senha deve ter pelo menos 6 caracteres').required('Senha é obrigatória'),
-    otherwise: (schema) => schema.notRequired(),
-  }),
-  confirmPassword: Yup.string().when('changePassword', {
-    is: true,
-    then: (schema) =>
-      schema
-        .required('Confirmação de senha é obrigatória')
-        .oneOf([Yup.ref('password')], 'As senhas não conferem'),
-    otherwise: (schema) => schema.notRequired(),
-  }),
-});
 
 const EditUserModal: React.FC<EditUserModalProps> = ({ visible, user, onClose, onSave }) => {
   return (
