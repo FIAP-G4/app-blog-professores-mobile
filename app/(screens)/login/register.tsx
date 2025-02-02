2b4b2818f4a4ed396b5075a680bb40667acc5b5b
pedrovbo <pedrovicboliveira@gmail.com>
2025-02-02 10:25:58 -0300

    remoção de link para login dentro do register

--- app/(screens)/login/register.tsx
+++ app/(screens)/login/register.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Button, Text, TextInput, View, ActivityIndicator } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import { Link } from 'expo-router';
-import styles from './styles';
-import useCreateAccountForm from '@/app/utils/hooks/useCreateAccountForm';
-import Toast from 'react-native-toast-message';
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Button, Text, TextInput, View, ActivityIndicator } from 'react-native'
+import { Picker } from '@react-native-picker/picker'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+import { Link } from 'expo-router'
+import styles from './styles'
+import useCreateAccountForm from '@/app/utils/hooks/useCreateAccountForm'
+import Toast from 'react-native-toast-message'
 
 // Definição do esquema de validação com Yup
 const schema = Yup.object().shape({
@@ -18,10 +18,10 @@ const schema = Yup.object().shape({
   confirmPassword: Yup.string()
     .oneOf([Yup.ref('password')], 'As senhas não conferem')
     .required('Confirmação de senha é obrigatória'),
-});
+})
 
 export default function Register(): JSX.Element {
-  const { handleCreateUser, loading } = useCreateAccountForm();
+  const { handleCreateUser, loading } = useCreateAccountForm()
 
   return (
     <SafeAreaView style={styles.screen}>
@@ -54,51 +54,61 @@ export default function Register(): JSX.Element {
               <Picker
                 style={styles.input}
                 selectedValue={values.typeUser}
-                onValueChange={(itemValue) => setFieldValue('typeUser', itemValue)}
+                onValueChange={(itemValue) =>
+                  setFieldValue('typeUser', itemValue)
+                }
               >
-                <Picker.Item label="Professor" value="1" />
-                <Picker.Item label="Aluno" value="2" />
+                <Picker.Item label='Professor' value='1' />
+                <Picker.Item label='Aluno' value='2' />
               </Picker>
-              {touched.typeUser && errors.typeUser && <Text style={styles.error}>{errors.typeUser}</Text>}
+              {touched.typeUser && errors.typeUser && (
+                <Text style={styles.error}>{errors.typeUser}</Text>
+              )}
 
               <Text style={styles.label}>Nome</Text>
               <TextInput
                 style={styles.input}
                 value={values.name}
-                placeholder="Digite seu nome"
+                placeholder='Digite seu nome'
                 onChangeText={handleChange('name')}
                 onBlur={handleBlur('name')}
               />
-              {touched.name && errors.name && <Text style={styles.error}>{errors.name}</Text>}
+              {touched.name && errors.name && (
+                <Text style={styles.error}>{errors.name}</Text>
+              )}
 
               <Text style={styles.label}>E-mail</Text>
               <TextInput
                 style={styles.input}
                 value={values.email}
-                placeholder="Digite seu e-mail"
+                placeholder='Digite seu e-mail'
                 onChangeText={handleChange('email')}
                 onBlur={handleBlur('email')}
-                keyboardType="email-address"
+                keyboardType='email-address'
               />
-              {touched.email && errors.email && <Text style={styles.error}>{errors.email}</Text>}
+              {touched.email && errors.email && (
+                <Text style={styles.error}>{errors.email}</Text>
+              )}
 
               <Text style={styles.label}>Senha</Text>
               <TextInput
                 style={styles.input}
                 secureTextEntry
                 value={values.password}
-                placeholder="Digite sua senha"
+                placeholder='Digite sua senha'
                 onChangeText={handleChange('password')}
                 onBlur={handleBlur('password')}
               />
-              {touched.password && errors.password && <Text style={styles.error}>{errors.password}</Text>}
+              {touched.password && errors.password && (
+                <Text style={styles.error}>{errors.password}</Text>
+              )}
 
               <Text style={styles.label}>Confirmar Senha</Text>
               <TextInput
                 style={styles.input}
                 secureTextEntry
                 value={values.confirmPassword}
-                placeholder="Confirme sua senha"
+                placeholder='Confirme sua senha'
                 onChangeText={handleChange('confirmPassword')}
                 onBlur={handleBlur('confirmPassword')}
               />
@@ -108,25 +118,20 @@ export default function Register(): JSX.Element {
 
               <View style={styles.buttonContainer}>
                 {loading ? (
-                  <ActivityIndicator size="large" color="#4e46dd" />
+                  <ActivityIndicator size='large' color='#4e46dd' />
                 ) : (
-                  <Button title="Registrar" color="#4e46dd" onPress={handleSubmit as any} />
+                  <Button
+                    title='Registrar'
+                    color='#4e46dd'
+                    onPress={handleSubmit as any}
+                  />
                 )}
               </View>
             </>
           )}
         </Formik>
-
-        <View style={styles.buttonContainer}>
-          <Text style={{ marginTop: -1, fontSize: 15 }}>
-            Já possui uma conta?{' '}
-            <Link href="/login" style={{ color: 'rgb(78, 70, 221)' }}>
-              Faça login.
-            </Link>
-          </Text>
-        </View>
       </View>
       <Toast />
     </SafeAreaView>
-  );
+  )
 }
