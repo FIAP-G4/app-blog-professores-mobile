3daeeeb7db50e220634f4a4634a6d4ed1597aff7
pedrovbo <pedrovicboliveira@gmail.com>
2025-02-03 23:34:14 -0300

    fix de expansão quando ocorre erro de preenchimento do form

--- app/(screens)/login/styles.ts
+++ app/(screens)/login/styles.ts
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 1,
     borderColor: '#ccc',
-    marginBottom: 20,
+    marginBottom: 10,
   },
   buttonContainer: {
     marginBottom: 20,
@@ -58,7 +58,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     borderWidth: 1,
-    marginBottom: 20,
+    marginBottom: 10,
     borderColor: '#ccc',
     borderRadius: 10,
     paddingHorizontal: 10,
@@ -70,6 +70,7 @@ const styles = StyleSheet.create({
   error: {
     color: 'red',
     marginBottom: 10,
+    minHeight: 20, // Evita que o layout "salte" quando a mensagem aparece
   },
 })
 
