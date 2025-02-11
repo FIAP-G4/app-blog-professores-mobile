69176990fff3a61c88066304ccc1a5939fd71f59
pedrovbo <pedrovicboliveira@gmail.com>
2025-02-11 09:39:02 -0300

    adição de navigation container

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,16 +3,19 @@ import Toast from 'react-native-toast-message'
 import { Slot } from 'expo-router'
 import { StyleSheet, View } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native'
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <AuthProvider>
-        <Slot />
-        <View style={styles.toastContainer}>
-          <Toast />
-        </View>
-      </AuthProvider>
+      <NavigationContainer>
+        <AuthProvider>
+          <Slot />
+          <View style={styles.toastContainer}>
+            <Toast />
+          </View>
+        </AuthProvider>
+      </NavigationContainer>
     </GestureHandlerRootView>
   )
 }
