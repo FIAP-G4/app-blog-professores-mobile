e69d124cb4b95561659552e486d1c740304946cc
thiagotnaraujo <thago@crowdability.com>
2025-02-13 17:16:10 -0300

    Conserto da imagem do card no iPhone

--- app/components/CardPost/styles.ts
+++ app/components/CardPost/styles.ts
@@ -1,4 +1,4 @@
-import { Platform, StyleSheet } from 'react-native'
+import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
   card: {
@@ -9,7 +9,6 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderRadius: 8,
     width: '90%',
-    // minHeight: 250,
     flexGrow: 1,
     justifyContent: 'space-between',
     overflow: 'hidden',
@@ -20,7 +19,7 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     flexGrow: 1,
     justifyContent: 'space-between',
-    width: Platform.OS == 'ios' ? '90%' : '100%',
+    width: '100%',
     overflow: 'hidden',
   },
   cardImageWrapper: {
