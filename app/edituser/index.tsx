d78ad1933910f0cd59613fec5937c472e5d4d8b4
thiagotnaraujo <thago@crowdability.com>
2025-02-13 19:09:56 -0300

    Botões de atualizar usuário padronizado

--- app/edituser/index.tsx
+++ app/edituser/index.tsx
@@ -1,39 +1,43 @@
-import React from 'react';
-import { View, Text, TextInput, Switch, SafeAreaView } from 'react-native';
-import { Formik } from 'formik';
-import { useRouter, useLocalSearchParams } from 'expo-router';
-import styles from './styles';
-import principalStyles from '@/app/styles';
-import schema from './schema';
-import { ITeacher } from '@/app/utils/hooks/useTeacherList';
-import useUpdateUser from '@/app/utils/hooks/useUserUpdate';
-import { IStudent } from '@/app/utils/hooks/useStudentList';
-import Header from '../components/shared/Header';
-import { Button } from 'react-native-paper';
+import React from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  Switch,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native'
+import { Formik } from 'formik'
+import { useRouter, useLocalSearchParams } from 'expo-router'
+import styles from './styles'
+import principalStyles from '@/app/styles'
+import schema from './schema'
+import { ITeacher } from '@/app/utils/hooks/useTeacherList'
+import useUpdateUser from '@/app/utils/hooks/useUserUpdate'
+import { IStudent } from '@/app/utils/hooks/useStudentList'
+import Header from '../components/shared/Header'
 
-export default function (): JSX.Element {
-  const router = useRouter();
-  const { user, typeUser } = useLocalSearchParams();
-  const { handleUpdateUser } = useUpdateUser();
+export default function EditUser(): JSX.Element {
+  const router = useRouter()
+  const { user, typeUser } = useLocalSearchParams()
+  const { handleUpdateUser } = useUpdateUser()
 
-  let parsedUser: ITeacher | IStudent | null = null;
+  let parsedUser: ITeacher | IStudent | null = null
   try {
-    parsedUser = user ? JSON.parse(user as string) : null;
+    parsedUser = user ? JSON.parse(user as string) : null
   } catch (error) {
-    console.error("Error parsing user object:", error);
-    router.back(); 
+    console.error('Error parsing user object:', error)
+    router.back()
     return <Text>Error loading user data.</Text>
   }
 
   if (!parsedUser) {
-    return <Text>No user data provided.</Text>;
+    return <Text>No user data provided.</Text>
   }
 
   return (
-    <SafeAreaView style={{  backgroundColor: '#f3f4f6',height:'100%' }} >
-      <Header
-        pageTitle='Blog Escolar'
-      />
+    <SafeAreaView style={{ backgroundColor: '#f3f4f6', height: '100%' }}>
+      <Header pageTitle="Blog Escolar" />
       <View style={principalStyles.subHeader}>
         <Text style={principalStyles.pageTitle}>Editar usuario</Text>
       </View>
@@ -54,12 +58,12 @@ export default function (): JSX.Element {
             name: values.name,
             email: values.email,
             ...(values.changePassword ? { password: values.password } : {}),
-          };
+          }
 
-          await handleUpdateUser(updatedUser.user_id, updatedUser);
-          console.log("User updated:", updatedUser);
-          if(typeUser === 'teacher') router.push('/(screens)/teacher');
-          if(typeUser === 'student') router.push('/(screens)/student');
+          await handleUpdateUser(updatedUser.user_id, updatedUser)
+          console.log('User updated:', updatedUser)
+          if (typeUser === 'teacher') router.push('/(screens)/teacher')
+          if (typeUser === 'student') router.push('/(screens)/student')
         }}
       >
         {({
@@ -72,23 +76,25 @@ export default function (): JSX.Element {
           setFieldValue,
           validateForm,
         }) => (
-          <View   style={principalStyles.screen}>
+          <View style={principalStyles.screen}>
             <View style={{ marginBottom: 10 }}>
               <Text style={styles.label}>Nome</Text>
-              <TextInput 
-                style={styles.input}                 
+              <TextInput
+                style={styles.input}
                 value={values.name}
                 placeholder="Digite seu nome"
                 onChangeText={handleChange('name')}
-                onBlur={handleBlur('name')} 
+                onBlur={handleBlur('name')}
                 placeholderTextColor={'#888'}
               />
-              {touched.name && errors.name && <Text style={styles.error}>{errors.name}</Text>}
+              {touched.name && errors.name && (
+                <Text style={styles.error}>{errors.name}</Text>
+              )}
             </View>
 
             <View style={{ marginBottom: 10 }}>
               <Text style={styles.label}>E-mail</Text>
-              <TextInput                 
+              <TextInput
                 style={styles.input}
                 value={values.email}
                 placeholder="Digite seu e-mail"
@@ -97,22 +103,32 @@ export default function (): JSX.Element {
                 keyboardType="email-address"
                 placeholderTextColor={'#888'}
               />
-              {touched.email && errors.email && <Text style={styles.error}>{errors.email}</Text>}
+              {touched.email && errors.email && (
+                <Text style={styles.error}>{errors.email}</Text>
+              )}
             </View>
 
-            <View style={{ marginBottom: 10, justifyContent: 'flex-start', flexDirection: 'row', alignItems: 'center' }}>
-              <Text style={{ fontSize: 14, fontWeight: "500", color: "#111827" }}>
-                Deseja alterar a senha?     
-                </Text>        
-                 <Switch 
-                  style={{ marginLeft: 10 }}
-                  value={values.changePassword} 
-                  onValueChange={(value) => {
-                    setFieldValue('changePassword', value);
-                    validateForm();
-                  }} 
+            <View
+              style={{
+                marginBottom: 10,
+                justifyContent: 'flex-start',
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}
+            >
+              <Text
+                style={{ fontSize: 14, fontWeight: '500', color: '#111827' }}
+              >
+                Deseja alterar a senha?
+              </Text>
+              <Switch
+                style={{ marginLeft: 10 }}
+                value={values.changePassword}
+                onValueChange={(value) => {
+                  setFieldValue('changePassword', value)
+                  validateForm()
+                }}
               />
-
             </View>
 
             {values.changePassword && (
@@ -125,10 +141,12 @@ export default function (): JSX.Element {
                     value={values.password}
                     placeholder="Digite sua senha"
                     onChangeText={handleChange('password')}
-                    onBlur={handleBlur('password')} 
+                    onBlur={handleBlur('password')}
                     placeholderTextColor={'#888'}
                   />
-                  {touched.password && errors.password && <Text style={styles.error}>{errors.password}</Text>}
+                  {touched.password && errors.password && (
+                    <Text style={styles.error}>{errors.password}</Text>
+                  )}
                 </View>
 
                 <View style={{ marginBottom: 12 }}>
@@ -139,7 +157,7 @@ export default function (): JSX.Element {
                     value={values.confirmPassword}
                     placeholder="Confirme sua senha"
                     onChangeText={handleChange('confirmPassword')}
-                    onBlur={handleBlur('confirmPassword')} 
+                    onBlur={handleBlur('confirmPassword')}
                     placeholderTextColor={'#888'}
                   />
                   {touched.confirmPassword && errors.confirmPassword && (
@@ -147,31 +165,25 @@ export default function (): JSX.Element {
                   )}
                 </View>
               </View>
-            )}  
+            )}
 
             <View style={styles.buttonContainer}>
-                <View style={styles.buttonWrapper}>
-                  <Button
-                    onPress={() => router.back()}
-                    mode='contained'
-                    buttonColor='red'
-                  >
-                    Cancelar
-                  </Button>
-                </View>
-                <View style={styles.buttonWrapper}>
-                  <Button
-                    onPress={handleSubmit as any}
-                    mode='contained'
-                    buttonColor='#4e46dd'
-                  >
-                    Salvar
-                  </Button>
-                </View>
+              <View
+                style={[styles.buttonWrapper, { backgroundColor: '#dd4646' }]}
+              >
+                <TouchableOpacity onPress={() => router.back()}>
+                  <Text style={styles.buttonText}>Cancelar</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.buttonWrapper}>
+                <TouchableOpacity onPress={() => handleSubmit()}>
+                  <Text style={styles.buttonText}>Salvar</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
         )}
       </Formik>
     </SafeAreaView>
-  );
-};
+  )
+}
