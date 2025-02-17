91372b0ed950b942f1c5f2b43fe7ecbdbdd5a3d1
pedrovbo <pedrovicboliveira@gmail.com>
2025-02-17 20:53:44 -0300

    Tratativa para quando não há postagens

--- app/(screens)/postagens/index.tsx
+++ app/(screens)/postagens/index.tsx
@@ -9,6 +9,7 @@ import {
   TextInput,
   TouchableOpacity,
   ActivityIndicator,
+  Text,
 } from 'react-native'
 import CustomMultipleSelectList from '@/app/components/CustomMultipleSelectList'
 import CardPost from '@/app/components/CardPost'
@@ -44,10 +45,10 @@ export default function Posts(): JSX.Element {
         <CustomMultipleSelectList
           setSelected={(val: any) => setSelected(val)}
           data={categoryOptions}
-          save="key"
-          label="Categorias"
-          placeholder="Buscar por categorias"
-          searchPlaceholder="Filtre por categoria"
+          save='key'
+          label='Categorias'
+          placeholder='Buscar por categorias'
+          searchPlaceholder='Filtre por categoria'
           boxStyles={styles.optionSelect}
           dropdownStyles={styles.dropdown}
           badgeStyles={styles.badgeStyles}
@@ -57,7 +58,7 @@ export default function Posts(): JSX.Element {
       <View style={styles.textInputWrapper}>
         <TextInput
           style={styles.textInput}
-          placeholder="Buscar por postagens"
+          placeholder='Buscar por postagens'
           onChangeText={(value) => setSearchTerm(value)}
           value={searchTerm}
         />
@@ -69,11 +70,16 @@ export default function Posts(): JSX.Element {
               setCurrentPage(1)
             }}
           >
-            <AntDesign name="search1" size={24} color="#FFFFFF" />
+            <AntDesign name='search1' size={24} color='#FFFFFF' />
           </TouchableOpacity>
         </View>
       </View>
-      {loading && <ActivityIndicator size="large" color="#0000ff" />}
+      {loading && <ActivityIndicator size='large' color='#0000ff' />}
+      {!loading && posts.length === 0 && (
+        <View style={styles.noPostsContainer}>
+          <Text style={styles.noPostsText}>Ainda não há postagens...</Text>
+        </View>
+      )}
       <FlatList
         data={posts}
         renderItem={({ item }) => {
--- app/(screens)/postagens/styles.ts
+++ app/(screens)/postagens/styles.ts
@@ -92,6 +92,15 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
   },
+  noPostsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  noPostsText: {
+    fontSize: 18,
+    color: '#000',
+  },
 })
 
 export default styles
